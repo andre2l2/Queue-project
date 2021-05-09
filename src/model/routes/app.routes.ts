1aeeffb88c416a5bd0a787e2484ff7ad8d1916da
Andre Ferreira <delcferreira@gmail.com>
2021-05-08 22:40:42 -0300

    refactory

--- src/model/routes/app.routes.ts
+++ src/model/routes/app.routes.ts
@@ -3,10 +3,16 @@ import cors from 'cors';
 import QueueContoller from '../../controller/queue.controller';
 
 const PORT = 3333;
-
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '../../../public'));
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (req, res) => {
+    res.render('./pages/index');
+})
 
 app.get('/queue', QueueContoller.getAll);  
 app.put('/queue/:id', QueueContoller.update);
@@ -15,4 +21,4 @@ app.delete('/queue/:id', QueueContoller.delete);
 
 export const listen = app.listen(PORT, () => 
     console.log(`\nVocê deve estar procurando a porta. Ela tá aqui! -> Running in ${PORT}`)
-);
+);  
