514b03a950aa344307533243e74a904c6fae1a2a
Andre Ferreira <delcferreira@gmail.com>
2021-09-15 22:01:37 -0300

    :recycle: ajustments and setings pretierrc

--- src/model/routes/app.routes.ts
+++ src/model/routes/app.routes.ts
@@ -5,7 +5,6 @@ import QueueContoller from '../../controller/queue.controller';
 import { PublicContoller } from '../../controller/public.contoller';
 import { SocketUtil } from '../aplication/socket';
 
-
 const PORT = 3333;
 const app = express();
 
@@ -15,21 +14,19 @@ app.use(express.static('./public'));
 app.use(express.json());
 app.use(cors());
 
-app.get('/', PublicContoller.home)
-app.get('/admin', PublicContoller.admin)
-app.get('/client', PublicContoller.client)
+app.get('/', PublicContoller.home);
+app.get('/admin', PublicContoller.admin);
+app.get('/client', PublicContoller.client);
 
-app.get('/queue', QueueContoller.getAll);  
+app.get('/queue', QueueContoller.getAll);
 app.put('/queue/:id', QueueContoller.update);
 app.post('/queue', QueueContoller.create);
 app.delete('/queue/:id', QueueContoller.delete);
 
-export const listen = app.listen(PORT, () => 
-    console.log(`\nHello! Running in ${PORT}`)
-);  
+export const listen = app.listen(PORT, () => console.log(`\n Hello! Running in ${PORT}`));
 
 const io = new Server(listen);
-io.on('connection', (socket) => {    
-    SocketUtil.get(socket);
-    SocketUtil.send(socket);
-})
+io.on('connection', (socket) => {
+	SocketUtil.get(socket);
+	SocketUtil.send(socket);
+});
